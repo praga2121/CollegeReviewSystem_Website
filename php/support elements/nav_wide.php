e5b6334ab23ddbc52a269a20473e2e3ffeebf7b6
Daniel <72720469+IAmDanieI@users.noreply.github.com>
2021-10-13 23:07:55 +0800

    Linking Issue Fixed Again

--- php/support elements/nav_wide.php
+++ php/support elements/nav_wide.php
@@ -6,7 +6,7 @@ echo '<ul>';
 //echo '<li style="float: right;"><a class="nav-menu" href="https://localhost/CollegeCapstone-main/php/admin%20panel/login.php">Log-In</a></li>';
 
 //for testing purposes
-echo '<li><a href="../php/main.php">ReviewMyCollege</a></li>';
+echo '<li><a href="../main.php">ReviewMyCollege</a></li>';
 echo '<li style="float: right;"><a class="nav-menu" href="../php/admin%20panel/login.php">Log-In</a></li>';
 echo '</ul>';
 echo '';
