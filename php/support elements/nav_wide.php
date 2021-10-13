15bf75c152a677ac0b480d60ab2f4cf5665d1576
Daniel <72720469+IAmDanieI@users.noreply.github.com>
2021-10-13 22:41:09 +0800

    Testing NGROK

--- php/support elements/nav_wide.php
+++ php/support elements/nav_wide.php
@@ -2,7 +2,11 @@
 echo '<link rel="stylesheet" href="../../css/style-main.css"/>';
 echo '<!--Navigation bar-->';
 echo '<ul>';
-echo '<li><a href="https://localhost/CollegeReview/php/main.php">ReviewMyCollege</a></li>';
-echo '<li style="float: right;"><a class="nav-menu" href="https://localhost/CollegeReview/php/admin%20panel/login.php">Log-In</a></li>';
+//echo '<li><a href="https://localhost/CollegeCapstone-main/php/main.php">ReviewMyCollege</a></li>';
+//echo '<li style="float: right;"><a class="nav-menu" href="https://localhost/CollegeCapstone-main/php/admin%20panel/login.php">Log-In</a></li>';
+
+//for testing purposes
+echo '<li><a href="main.php">ReviewMyCollege</a></li>';
+echo '<li style="float: right;"><a class="nav-menu" href="admin%20panel/login.php">Log-In</a></li>';
 echo '</ul>';
 echo '';
