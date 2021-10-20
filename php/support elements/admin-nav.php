f5c7cae698baf46f5c4e931ab06d538aceb7155c
ORM-Laptop <knightdeman@gmail.com>
2021-10-20 01:18:49 -0700

    one little bugged/lost boy

--- php/support elements/admin-nav.php
+++ php/support elements/admin-nav.php
@@ -4,11 +4,7 @@ echo '<link rel="stylesheet" href="../../css/style-admin-nav.css"/>';
 echo '<!--Navigation bar-->';
 echo '<ul>';
 echo '<li><a href="https://localhost/CollegeCapstone/php/main.php">CollegeReport</a></li>';
-<<<<<<< HEAD
-echo '<li style="float: right;"><a class="nav-menu" href="https://localhost/CollegeCapstone/php/admin%20panel/login.php">Log-In</a></li>';
-=======
 echo '<li style="float: right;"><a class="nav-menu" href="https://localhost/CollegeCapstone/php/admin%20panel/listing.php">Admin</a></li>';
->>>>>>> c5a548222a834e6efd19ec3c2d41f80ccc429bfd
 
 //for testing purposes
 //echo '<li><a href="..php/main.php">CollegeReport</a></li>';
