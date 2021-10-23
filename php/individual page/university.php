683189b5c8eb101e86130af0dd8161a8ae5258a7
ORM-Laptop <knightdeman@gmail.com>
2021-10-23 11:11:31 -0700

    google map fix

--- php/individual page/university.php
+++ php/individual page/university.php
@@ -47,7 +47,7 @@ echo '<div style="width: 100%">
         scrolling="no" 
         marginheight="0" 
         marginwidth="0" 
-        src="https://maps.google.com/maps?width=100%25&amp;height=500&amp;hl=en&amp;q=+('.$row["name"].'Disted%20College)&amp;t=p&amp;z=15&amp;ie=UTF8&amp;iwloc=B&amp;output=embed" 
+        src="https://maps.google.com/maps?width=100%25&amp;height=500&amp;hl=en&amp;q=+('.$row["name"].' Malaysia)&amp;t=p&amp;z=15&amp;ie=UTF8&amp;iwloc=B&amp;output=embed" 
         width="100%" 
         height="600" 
         frameborder="0">
