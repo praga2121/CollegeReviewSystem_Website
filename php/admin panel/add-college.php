156a9431670e26267e029467837b9b559f05f9e7
ORM-Laptop <knightdeman@gmail.com>
2021-10-24 02:39:45 -0700

    yeeeted useless file

--- php/admin panel/add-college.php
+++ php/admin panel/add-college.php
@@ -11,9 +11,9 @@ try {
   }
 
 if (isset($_POST['added'])) {
-    $college_name = mysqli_real_escape_string($_POST["name"]);
-    $url =  mysqli_real_escape_string($_POST["websitelink"]);
-    $college_description = mysqli_real_escape_string($_POST["description"]);
+    $college_name = $_POST["name"];
+    $url =  $_POST["websitelink"];
+    $college_description = $_POST["description"];
     $subjects = $_POST["subject"];
     $subjects = array_values(array_filter($subjects, 'array_filter'));
 
@@ -21,14 +21,14 @@ if (isset($_POST['added'])) {
     $query_college = "INSERT INTO colleges 
                       (name, college_description,url) 
                       VALUES (
-                            '$college_name', '$college_description','$url'
+                            {$college_name}, {$college_description},{$url}
                       )";
     $query_subjects = "INSERT INTO collegesandsubjects 
                       (college_id, subject_id, price)
                       VALUES (
                         (SELECT college_id 
                           FROM colleges 
-                          WHERE name='$college_name' 
+                          WHERE name={$college_name} 
                           LIMIT 1), 
                         ?, 
                         ?
