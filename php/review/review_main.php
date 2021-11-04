4c4f024dbcc56c4ed40067c571145e3cd4bc119c
ORM-Laptop <knightdeman@gmail.com>
2021-11-04 04:31:55 -0700

    fix college subjects error

--- php/review/review_main.php
+++ php/review/review_main.php
@@ -22,8 +22,10 @@
 
 
 		<script>
+		// useless stuff that I am too lazy to clean up
 		const reviews_page_id = 1;
 
+		
 		fetch("reviews.php?page_id=" + reviews_page_id).then(response => response.text()).then(data => {
 			document.querySelector(".reviews").innerHTML = data;
 			document.querySelector(".reviews .write_review_btn").onclick = event => {
@@ -33,6 +35,7 @@
 			};
 			document.querySelector(".reviews .write_review form").onsubmit = event => {
 				event.preventDefault();
+				// 
 				fetch("reviews.php?page_id=" + reviews_page_id, {
 					method: 'POST',
 					body: new FormData(document.querySelector(".reviews .write_review form"))
